f8d0a6fb5f4646c8e429386685e444ac346e6197
Diogo Folador <folador.diogo@gmail.com>
2022-09-03 18:49:18 -0300

    import only some modules from vscode, TODO code

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+// Import only some of the modules from vscode
+import { ConfigurationTarget, ExtensionContext, window, workspace } from 'vscode'
+import { RichQuickPickItem, ToggleConfig, OnOff } from './types'
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
