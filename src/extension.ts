c8259d79f5fe910a94758df25203bb9775226950
Diogo Folador <folador.diogo@gmail.com>
2022-09-03 19:04:04 -0300

    correct references

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,7 @@ import { RichQuickPickItem, ToggleConfig, OnOff } from './types'
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
     
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
@@ -14,10 +14,10 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('vscode-settings-switcher.helloWorld', () => {
+    let disposable = commands.registerCommand('vscode-settings-switcher.helloWorld', () => {
         // The code you place here will be executed every time your command is executed
         // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from VSCode Settings Switcher!');
+        window.showInformationMessage('Hello World from VSCode Settings Switcher!');
     });
 
     context.subscriptions.push(disposable);
