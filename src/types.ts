d39bc4ef7a6ebee5728117f15b3922b709b94d73
Diogo Folador <folador.diogo@gmail.com>
2022-09-03 18:41:56 -0300

    include `types` from vscode-settings-on-fire

---
+++ src/types.ts
@@ -0,0 +1,22 @@
+import { ConfigurationTarget, QuickPickItem, Memento } from 'vscode'
+
+export type OnOff = 'on' | 'off'
+
+type Config = {
+    _label: string
+    [setting: string]: any
+}
+
+export type ToggleConfig = {
+    [name: string]: {
+        on: Config
+        off: Config
+    }
+}
+
+export type RichQuickPickItem = QuickPickItem & {
+    name: string
+    newState: OnOff
+    configTarget: ConfigurationTarget
+    store: Memento
+}
