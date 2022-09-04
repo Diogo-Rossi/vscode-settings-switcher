7cebae4777545524d2e9b7ec47d10857425bfb6d
Diogo Folador <folador.diogo@gmail.com>
2022-09-03 21:52:37 -0300

    index file

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { commands, ExtensionContext, window } from 'vscode'
+import { toggleSettings } from './toggleSettings'
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export const activate = async function activate(context: ExtensionContext) {
+    context.subscriptions.push(
+        commands.registerCommand('settingsOnFire.toggleSettings', () => toggleSettings(context))
+    )
+}
