507cdb979ef164e0ed74c9c393d0c14157361f48
Diogo Folador <folador.diogo@gmail.com>
2023-07-30 23:04:32 -0300

    correct condition in previous commit to publish
    even working, typescript was raising error:
    "Spread types may only be created from object types."

--- src/switchSettings.ts
+++ src/switchSettings.ts
@@ -40,7 +40,11 @@ export async function switchSettings(context: ExtensionContext) {
         // Merge objects, overwrite other types
         if (val && typeof val === 'object' && !Array.isArray(val)) {
             if (!currentConfig || typeof currentConfig === 'object') {
-                newConfig = { ...currentConfig, ...val }
+                if (currentConfig === undefined){
+                    newConfig = val
+                }else{
+                    newConfig = { ...currentConfig, ...val }
+                }
             } else {
                 window.showErrorMessage(
                     'Settings Switcher error! Toggle configuration specified is a different type than the existing one.',
